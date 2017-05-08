1bc079c6ce48df4862fc1f0aa091c9a22de558e9
Mr.Yang <paul.wide@gmail.com>
2017-05-08 10:41:56 +0800

    添加spring cloud config

---
+++ cloud-config-client/src/main/java/com/zz/config/client/ConfigClientApplication.java
@@ -0,0 +1,29 @@
+package com.zz.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mr.Yang
+ * @since 2017-05-08
+ */
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${name:default}")
+    private String name;
+
+    @RequestMapping(value = "/hello")
+    public String hello() {
+        return name;
+    }
+}
---
+++ cloud-config-server/src/main/java/com/zz/config/server/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.zz.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Mr.Yang
+ * @since 2017-05-07
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
