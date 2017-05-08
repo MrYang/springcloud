1c81550125726b3d4fed3dbe1c063f6624f2c91f
Mr.Yang <paul.wide@gmail.com>
2017-05-08 10:58:20 +0800

    添加config client 的start-web 支持

--- cloud-config-client/src/main/java/com/zz/config/client/ConfigClientApplication.java
+++ cloud-config-client/src/main/java/com/zz/config/client/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package com.zz.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
